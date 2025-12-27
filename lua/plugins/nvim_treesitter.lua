232d0ee20412fe0127ffbe8230cd04b90e99e2c4
compilercomplied <8614035+compilercomplied@users.noreply.github.com>
2025-12-28 00:30:05 +0100

    better treesitter setup

--- lua/plugins/nvim_treesitter.lua
+++ lua/plugins/nvim_treesitter.lua
@@ -1,8 +1,28 @@
-require("nvim-treesitter").install {
-	"nu", "yaml", "markdown", "go", "rust", "typescript", "python"
+---@type table<string, string> Key: parser name, Value: filetype pattern
+local languages = {
+	css = "css",
+	go = "go",
+	html = "html",
+	json = "json",
+	markdown = "md",
+	lua = "lua",
+	nu = "nu",
+	python = "py",
+	rust = "rs",
+	typescript = "ts",
+	yaml = "yaml",
 }
 
-vim.api.nvim_create_autocmd('FileType', {
-	pattern = { 'nu' },
-	callback = function() vim.treesitter.start() end,
-})
+local to_install = {}
+
+for parser, filetype in pairs(languages) do
+	table.insert(to_install, parser)
+	vim.api.nvim_create_autocmd('FileType', {
+		pattern = filetype,
+		callback = function()
+			vim.treesitter.start()
+		end,
+	})
+end
+
+require("nvim-treesitter").install(to_install)
