186f437260270c5f1eaef8244dcabd747497f2b9
compilercomplied <8614035+compilercomplied@users.noreply.github.com>
2026-01-01 10:35:47 +0100

    add support for multiple filetypes

--- lua/plugins/nvim_treesitter.lua
+++ lua/plugins/nvim_treesitter.lua
@@ -1,28 +1,30 @@
----@type table<string, string> Key: parser name, Value: filetype pattern
+---@type table<string, string[]> Key: parser name, Value: filetype patterns
 local languages = {
-	css = "css",
-	go = "go",
-	html = "html",
-	json = "json",
-	markdown = "md",
-	lua = "lua",
-	nu = "nu",
-	python = "py",
-	rust = "rs",
-	typescript = "ts",
-	yaml = "yaml",
+	css = { "css" },
+	go = { "go" },
+	html = { "html" },
+	json = { "json" },
+	markdown = { "md" },
+	lua = { "lua" },
+	nu = { "nu" },
+	python = { "py" },
+	rust = { "rs" },
+	typescript = { "ts" },
+	yaml = { "yaml", "yml" },
 }
 
 local to_install = {}
 
-for parser, filetype in pairs(languages) do
+for parser, filetypes in pairs(languages) do
 	table.insert(to_install, parser)
-	vim.api.nvim_create_autocmd('FileType', {
-		pattern = filetype,
-		callback = function()
-			vim.treesitter.start()
-		end,
-	})
+	for _, filetype in ipairs(filetypes) do
+		vim.api.nvim_create_autocmd('FileType', {
+			pattern = filetype,
+			callback = function()
+				vim.treesitter.start()
+			end,
+		})
+	end
 end
 
 require("nvim-treesitter").install(to_install)
