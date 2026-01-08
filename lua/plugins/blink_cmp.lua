bd52562cf8f8b233504ac7865d5148901c46bac6
compilercomplied <8614035+compilercomplied@users.noreply.github.com>
2026-01-08 09:05:35 +0100

    Add documentation to completion engine

--- lua/plugins/blink_cmp.lua
+++ lua/plugins/blink_cmp.lua
@@ -1,4 +1,10 @@
 require('blink.cmp').setup {
+	completion = {
+		documentation = {
+			auto_show = true,
+			auto_show_delay_ms = 500,
+		}
+	},
 	keymap = {
 		preset = 'default',
 		-- map 'tab' to jump to the next argument
