3e90f818e23ce7db718c162d1db67b18a7839bb4
compilercomplied <8614035+compilercomplied@users.noreply.github.com>
2026-01-31 10:25:40 +0100

    Fix bootstrapping

--- init.lua
+++ init.lua
@@ -1,7 +1,11 @@
 -- Force globals initialization first.
 require('editor_scripting.globals')
 
+-- plugin_manager has the bootstrapping configuration for the plugins
+-- we have to load it before  the rest of the modules so we can successfully
+-- bootstrap in a clean install.
+require('plugin_manager')
+
 require('editor')
 require('keymaps')
 require('lsp')
-require('plugin_manager')
